8f284be82908dc440f5def950c8826ba25813e72
Ilya.Komolkin <ilya.komolkin@jetbrains.com>
2020-06-05 14:08:24 +0300

    add draftJs test

--- src/test/java/com/iluxa/selenium/example/ChromeDriverTest.java
+++ src/test/java/com/iluxa/selenium/example/ChromeDriverTest.java
@@ -9,68 +9,69 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertTrue;
 
 public class ChromeDriverTest {
 
-    private String url;
     private WebDriver driver;
+    public final String expectedResult = "la-la-lo-la";
 
     @Before
-    public void prepare() throws MalformedURLException {
+    public void prepare() {
         System.setProperty(
                 "webdriver.chrome.driver",
                 "webdriver/chromedriver");
 
-        url = "https://google.com";
 
         final ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setHeadless(true);
+//        chromeOptions.setHeadless(true);
 
-//        driver = new ChromeDriver(chromeOptions);
-        driver = new RemoteWebDriver(new URL("http://circlet:circlet@ggr.labs.intellij.net:4444/wd/hub"), chromeOptions);
-        driver.get(url);
+        driver = new ChromeDriver(chromeOptions);
     }
 
     @Test
-    public void testInput() throws IOException {
-
-        List<WebElement> elements = driver
-                .findElements(By.cssSelector("input[name=q]"));
-
-        WebElement input = elements.get(0);
-        input.sendKeys("la-la-la");
-        Collections.nCopies(3, 1)
-                .stream()
-                .forEach(i ->
-                        elements.get(0).sendKeys(Keys.ARROW_LEFT));
-
-        input.sendKeys("-lo");
-
+    public void testGoogleInput() {
+        String url = "https://google.com";
+        String elementLocator = "input[name=q]";
+        driver.get(url);
+        WebElement element = driver.findElement(By.cssSelector(elementLocator));
+        test(element);
         assertTrue("input value is as expected",
                 (new WebDriverWait(driver, 5))
-                        .until(new ExpectedCondition<Boolean>() {
-                            public Boolean apply(WebDriver d) {
-                                return input.getAttribute("value").contains("la-la-lo-la");
-                            }
-                        })
+                        .until((ExpectedCondition<Boolean>) d -> element.getAttribute("value").contains(expectedResult))
         );
+    }
 
+    @Test
+    public void testDraftJs() {
+
+        String url = "https://draftjs.org/";
+        String elementLocator = "[contentEditable=true]";
+        driver.get(url);
+        WebElement element = driver.findElement(By.cssSelector(elementLocator));
+        test(element);
+        assertTrue("input value is as expected",
+                (new WebDriverWait(driver, 5))
+                        .until((ExpectedCondition<Boolean>) d -> element.getText().contains(expectedResult))
+        );
+    }
+
+    private void test(WebElement input){
+        input.click();
+        input.sendKeys("la-la-la");
+        Collections.nCopies(3, 1)
+                .forEach(i ->
+                        input.sendKeys(Keys.ARROW_LEFT));
+
+        input.sendKeys("-lo");
     }
 
     @After
-    public void teardown() throws IOException {
+    public void teardown() {
         driver.quit();
     }
 
